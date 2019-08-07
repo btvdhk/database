f61f19e0a26b689ae9f48e485e6727a589481057
sohan <btvdhk@gmail.com>
2019-08-07 06:23:13 +0600

    learned postgresql

--- script1.py
+++ script1.py
@@ -11,12 +11,12 @@ def insert(item,quantity,price):
     conn=psycopg2.connect("dbname='db1' user='postgres' password='50909' host='localhost' port='5432'")
     cur=conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
-    cur.execute("INSERT INTO store VALUES ('%s','%s','%s')" % (item,quantity,price))
+    cur.execute("INSERT INTO store VALUES (%s,%s,%s)", (item,quantity,price))
     conn.commit()
     conn.close()
 
 def view():
-    conn=psycopg2.connect("lite.db")
+    conn=psycopg2.connect("dbname='db1' user='postgres' password='50909' host='localhost' port='5432'")
     cur=conn.cursor()
     cur.execute("SELECT * FROM store")
     rows=cur.fetchall()
@@ -24,20 +24,23 @@ def view():
     return rows
 
 def delete(item):
-    conn=psycopg2.connect("lite.db")
+    conn=psycopg2.connect("dbname='db1' user='postgres' password='50909' host='localhost' port='5432'")
     cur=conn.cursor()
-    cur.execute("DELETE FROM store WHERE item=?",(item,))
+    cur.execute("DELETE FROM store WHERE item=%s",(item,))
     conn.commit()
     conn.close()
 
 def update(quantity,price,item):
-    conn=psycopg2.connect("lite.db")
+    conn=psycopg2.connect("dbname='db1' user='postgres' password='50909' host='localhost' port='5432'")
     cur=conn.cursor()
-    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?",(quantity,price,item))
+    cur.execute("UPDATE store SET quantity=%s, price=%s WHERE item=%s",(quantity,price,item))
     conn.commit()
     conn.close()
 
 create_table()
-inser("Apple",10,15)
+#insert("Orange",10,15)
+#delete("Orange")
+update(20,15.0,'Apple')
+print(view())
 #update(11,6,"water glass")
 #print(view())
